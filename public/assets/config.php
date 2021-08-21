a1cca978d41c2f29089685151f9fca829a4bf5dc
Monster2408 <meoto2408@gmail.com>
2021-08-21 21:39:34 +0900

    margin-top追加

--- public/assets/config.php
+++ public/assets/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 $path = "/home/users/2/main.jp-mls/web/www.mlserver.xyz/assets/config.php";
-if (str_pos($_SERVER["HTTP_HOST"], "localhost")) {
+if (strpos($_SERVER["HTTP_HOST"], "localhost")) {
     $path = "C:/newMLServerWeb/htdocs/main/public/assets/config.php";
 }
 include($path); 
--- public/assets/include/footer.php
+++ public/assets/include/footer.php
@@ -1,7 +1,7 @@
 <?php 
 
 $path = "/home/users/2/main.jp-mls/web/www.mlserver.xyz/assets/include/footer.php";
-if (str_pos($_SERVER["HTTP_HOST"], "localhost")) {
+if (strpos($_SERVER["HTTP_HOST"], "localhost")) {
     $path = "C:/newMLServerWeb/htdocs/main/public/assets/include/footer.php";
 }
 include($path); 
